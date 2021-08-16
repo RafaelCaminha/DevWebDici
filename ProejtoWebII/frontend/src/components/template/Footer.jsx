a49533adbcc906d542a19cae915873c58ab8690f
Rafael Camunha <rafaelcaminha@alu.ufc.br>
2021-08-16 15:53:13 -0300

    atualiação 16/08

---
+++ ProejtoWebII/frontend/src/components/template/Footer.jsx
@@ -0,0 +1,9 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className="footer">
+       <span>
+           Desenvolvido a base da raiva <i className="fas fa-angry text-danger"></i>
+       </span>
+    </footer>
---
+++ ProejtoWebII/frontend/src/components/template/Logo.jsx
@@ -0,0 +1,11 @@
+import './Logo.css'
+import logo from '../../assets/img/logo.jpg'
+
+import React from 'react'
+
+export default props =>
+    <aside className="logo">
+        <a href="/" className="logo">
+            <img src={logo} alt="logo"/>
+        </a>
+    </aside>
